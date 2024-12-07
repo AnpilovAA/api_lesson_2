df9c6c1a78dc6f62d272b043a03c4227e37b6ccd
Anpilov Andrey <a.anpilov93@gmail.com>
2024-12-07 09:48:48 +0300

    complited step 6

--- main.py
+++ main.py
@@ -10,8 +10,12 @@ def shorten_link(token: str, url: str) -> None:
     response = get(url=url, params=token)
     response.raise_for_status()
     if "error_code" in loads(response.text)['error']:
+        """"
+        Это условие помогает поймать ошибку в случае введения
+        пользователя ссылки в формате xx,ru
+        """
+        print(f"Код ошибки {response.status_code}, \nтекст: {response.text}")
         raise HTTPError
-    print(response.status_code, response.text)
     short_link = loads(response.text)["response"]["short_url"]
     return short_link
 
@@ -31,6 +35,6 @@ if __name__ == "__main__":
     }
     try:
         short_link = shorten_link(token=params, url=url)
+        print("Сокращенная ссылка: ", short_link)
     except HTTPError:
-        print(HTTPError)
-    print("Сокращенная ссылка: ", short_link)
+        print("Не правильная ссылка")
