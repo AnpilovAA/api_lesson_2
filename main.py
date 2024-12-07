92a14ec36159205754b16c7f14f6b535c9006680
Anpilov Andrey <a.anpilov93@gmail.com>
2024-12-07 14:55:43 +0300

    complited step 9

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 from json import loads
 from urllib.parse import urlparse
+from os import environ
 
-from config import TOKEN
 
+from dotenv import load_dotenv, find_dotenv
 from requests import get
 from requests.exceptions import HTTPError
 
 
 def shorten_link(token: str, url: str) -> None:
-    response = get(url=url, params=token)
-    response.raise_for_status()
-    if "error" in loads(response.text):
-        """"
-        Это условие помогает поймать ошибку в случае введения
-        пользователя ссылки в формате xx,ru
-        """
-        print(f"Код ошибки {response.status_code}, \nтекст: {response.text}")
-        raise HTTPError
-    short_link = loads(response.text)["response"]["short_url"]
-    return short_link
+    if is_shorten_link(token['url']):
+        return token['url']
+    else:
+        response = get(url=url, params=token)
+        response.raise_for_status()
+        if "error" in loads(response.text):
+            """"
+            these conditions help to detect the error
+            """
+            print(f"Код ошибки {response.status_code}, \nтекст: {response.text}")
+            raise HTTPError
+        short_link = loads(response.text)["response"]["short_url"]
+        return short_link
 
 
 def count_clicks(token: str, url: str):
@@ -26,33 +29,41 @@ def count_clicks(token: str, url: str):
     response.raise_for_status()
     if "error" in loads(response.text):
         """"
-        Это условие помогает поймать ошибку в случае введения
-        пользователя ссылки в формате xx,ru
+        these conditions help to detect the error
         """
         print(f"Код ошибки {response.status_code}\nтекст: {response.text}")
         raise HTTPError
     return loads(response.text)["response"]['stats'][0]["views"]
 
 
+def is_shorten_link(url: str) -> bool:
+    if urlparse(url).netloc == "vk.cc":
+        return True
+    return False
+
+
 if __name__ == "__main__":
+    load_dotenv(dotenv_path=find_dotenv())
     url = "https://api.vk.ru/method/utils.getShortLink"
     params = {
-        "access_token": TOKEN,
+        "access_token": environ["TOKEN"],
         # 'url': input("Вставьте ссылку: "),
-        'url': "hh.ru",
+        # 'url': "hh.ru",
+        'url': "https://vk.cc/131s0Y",
         'string': "key",
         "private": 0,
         "v": 5.199,
     }
     try:
         short_link = shorten_link(token=params, url=url)
+        print(short_link)
     except HTTPError:
         raise KeyboardInterrupt
 
     url_stat_method = "https://api.vk.ru/method/utils.getLinkStats"
     params_stat_method = {
         "key": urlparse(short_link).path.strip('/'),
-        "access_token": TOKEN,
+        "access_token": environ["TOKEN"],
         "v": 5.199,
     }
     try:
@@ -63,3 +74,7 @@ if __name__ == "__main__":
     except HTTPError:
         raise KeyboardInterrupt
     print(clicks_count)
+    if is_shorten_link(params["url"]):
+        print("ссылка короткая")
+    else:
+        print("ссылка длинная")
