c2d89a52a102899445f2e0902169f793a67b9ae0
Anpilov Andrey <a.anpilov93@gmail.com>
2024-12-22 11:48:28 +0300

    restructured if __name__

--- main.py
+++ main.py
@@ -9,20 +9,17 @@ from requests.exceptions import HTTPError
 
 
 def shorten_link(token: dict) -> str:
-    if is_shorten_link(token['url']):
-        return token['url']
-    else:
-        url = "https://api.vk.ru/method/utils.getShortLink"
-        response = get(url=url, params=token)
-        response.raise_for_status()
-        if "error" in loads(response.text):
-            """"
-            these conditions help to detect the error
-            """
-            print(f"Код ошибки {response.status_code}, \nтекст: {response.text}")
-            raise HTTPError
-        short_link = loads(response.text)["response"]["short_url"]
-        return short_link
+    url = "https://api.vk.ru/method/utils.getShortLink"
+    response = get(url=url, params=token)
+    response.raise_for_status()
+    if "error" in loads(response.text):
+        """"
+        these conditions help to detect the error
+        """
+        print(f"Код ошибки {response.status_code}, \nтекст: {response.text}")
+        raise HTTPError
+    short_link = loads(response.text)["response"]["short_url"]
+    return short_link
 
 
 def count_clicks(token: dict) -> str:
@@ -50,31 +47,33 @@ def is_shorten_link(url: str) -> bool:
 if __name__ == "__main__":
     load_dotenv(dotenv_path=find_dotenv())
     vk_token = environ["VK_TOKEN"]
-    params = {
+
+    params_short_link = {
         "access_token": vk_token,
         'url': input("Вставьте ссылку: "),
         'string': "key",
         "private": 0,
         "v": 5.199,
     }
-    try:
-        short_link = shorten_link(token=params)
-        print(short_link)
-    except HTTPError:
-        raise KeyboardInterrupt
+    link = params_short_link["url"]
 
-    params_stat_method = {
-        "key": urlparse(short_link).path.strip('/'),
-        "access_token": vk_token,
-        "v": 5.199,
-    }
-    try:
-        clicks_count = count_clicks(token=params_stat_method)
-    except HTTPError:
-        raise KeyboardInterrupt
-    if clicks_count is not None:
-        print("Количетсво кликов: ", clicks_count)
-    if is_shorten_link(params["url"]):
+    if is_shorten_link(link):
         print("Ссылка короткая")
+        params_stat_method = {
+            "key": urlparse(link).path.strip('/'),
+            "access_token": vk_token,
+            "v": 5.199,
+        }
+        try:
+            clicks_count = count_clicks(token=params_stat_method)
+        except HTTPError:
+            raise KeyboardInterrupt
+        if clicks_count is not None:
+            print("Количетсво кликов: ", clicks_count)
     else:
-        print("Ссылка длинная")
+        try:
+            print("Ссылка длинная")
+            short_link = shorten_link(token=params_short_link)
+            print(short_link)
+        except HTTPError:
+            raise KeyboardInterrupt
